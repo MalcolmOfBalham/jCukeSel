2e074ed89f95b85116d30352fc31e77c7f1ec82c
malc <malcolm@theperfects.co.uk>
2020-06-06 09:46:23 +0100

    initial pom

---
+++ src/test/java/com/jcukesel/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcukesel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
